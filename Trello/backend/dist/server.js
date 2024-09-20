15f82d0df940ddf80d39cef226d38d9551772b33
Willen Garcia <willen.garcia05@gmail.com>
2024-09-20 00:06:04 -0300

    subindo frontend

--- Trello/backend/dist/server.js
+++ Trello/backend/dist/server.js
@@ -10,9 +10,8 @@ const path_1 = __importDefault(require("path"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use((0, cors_1.default)({
-    origin: 'https://seu-frontend.com', // Substitua pelo domínio do seu front-end
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true
+    origin: '*', // Substitua pelo domínio do seu front-end
+    methods: ['GET', 'POST', 'PUT', 'DELETE']
 }));
 app.use(router_1.router);
 app.use('/files', express_1.default.static(path_1.default.resolve(__dirname, '..', 'tmp'))); // criar uma rota estática para exibir as fotos
--- Trello/backend/src/server.ts
+++ Trello/backend/src/server.ts
@@ -8,10 +8,13 @@ import path from "path";
 const app = express()
 
 app.use(express.json())
-app.use(cors({
-    origin: '*',  // Substitua pelo domínio do seu front-end
-    methods: ['GET', 'POST', 'PUT', 'DELETE']
-}))
+const corsOptions = {
+    origin: 'https://trello-o09frj28p-willen-garcia-s-projects.vercel.app/',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Métodos permitidos
+    credentials: true  // Permite cookies e credenciais
+  };
+// Aplicar o middleware de CORS
+app.use(cors(corsOptions));
 app.use(router)
 app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp'))) // criar uma rota estática para exibir as fotos
 app.listen(process.env.PORT, ()=>{console.log('Server Online!!')})
