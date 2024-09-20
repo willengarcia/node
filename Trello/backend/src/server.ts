f3100e90570f34656d8b5f8436bc6ceb62ddd05f
Willen Garcia <willen.garcia05@gmail.com>
2024-09-20 00:14:05 -0300

    testando cors para requisição

--- Trello/backend/src/server.ts
+++ Trello/backend/src/server.ts
@@ -1,20 +1,30 @@
-import express from 'express'
-import {router} from './router'
-import cors from 'cors'
-import path from "path";
+import express from 'express';
+import { router } from './router';
+import cors from 'cors';
+import path from 'path';
 
+const app = express();
 
-
-const app = express()
-
-app.use(express.json())
+// Middleware para permitir CORS
 const corsOptions = {
-    origin: 'https://trello-o09frj28p-willen-garcia-s-projects.vercel.app/',
+    origin: 'https://trello-o09frj28p-willen-garcia-s-projects.vercel.app', // Remova a barra final
     methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Métodos permitidos
     credentials: true  // Permite cookies e credenciais
-  };
+};
+
 // Aplicar o middleware de CORS
 app.use(cors(corsOptions));
-app.use(router)
-app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp'))) // criar uma rota estática para exibir as fotos
-app.listen(process.env.PORT, ()=>{console.log('Server Online!!')})
+
+// Middleware para processar JSON
+app.use(express.json());
+
+// Defina suas rotas aqui
+app.use(router);
+
+// Criar uma rota estática para exibir as fotos
+app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')));
+
+// Iniciar o servidor
+app.listen(process.env.PORT, () => {
+    console.log('Server Online!!');
+});
