1504c8a587cbe706553084bbafd6d96ecad95b15
Willen Garcia <89990936+willengarcia@users.noreply.github.com>
2024-09-20 15:35:06 -0300

    ordenando lojas manual

--- Trello/backend/src/services/storage/ListStorageService.ts
+++ Trello/backend/src/services/storage/ListStorageService.ts
@@ -1,7 +1,7 @@
 import prismaClient from "../../prisma";
 class ListStorageService{
     async execute(){
-        const userWithStore = await prismaClient.store.findMany({
+        let userWithStore = await prismaClient.store.findMany({
             include:{
                 users:true
             },
@@ -9,6 +9,13 @@ class ListStorageService{
                 name: 'asc' // Ordena alfabéticamente, mas você pode sobrescrever depois
             }
         })
+        // Processamento manual para ordenar por número
+        userWithStore = userWithStore.sort((a, b) => {
+            const numA = parseInt(a.name.replace(/\D/g, ''), 10); // Remove as letras e pega os números
+            const numB = parseInt(b.name.replace(/\D/g, ''), 10);
+            return numA - numB;
+        });
+  
         return userWithStore
     }
 }
