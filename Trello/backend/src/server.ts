2de39c7729dd90ed694b000bf73c841f6be4b3d6
Willen Garcia <willen.garcia05@gmail.com>
2024-09-20 01:30:54 -0300

    Update server.ts

--- Trello/backend/src/server.ts
+++ Trello/backend/src/server.ts
@@ -8,17 +8,23 @@ const app = express();
 // Middleware para permitir CORS
 const corsOptions = {
     origin: 'https://trello-six-rose.vercel.app', // Remova a barra final
-    methods: ['GET', 'POST', 'PUT', 'DELETE']  // Métodos permitidos
-    //credentials: true  // Permite cookies e credenciais
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
+    allowedHeaders: ['Content-Type', 'Authorization'], // Cabeçalhos permitidos
+    credentials: true, // Permite cookies e credenciais, se necessário
 };
+
 // Aplicar o middleware de CORS
 app.use(cors(corsOptions));
+
 // Middleware para processar JSON
 app.use(express.json());
+
 // Defina suas rotas aqui
 app.use(router);
+
 // Criar uma rota estática para exibir as fotos
 app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')));
+
 // Iniciar o servidor
 app.listen(process.env.PORT, () => {
     console.log('Server Online!!');
