05ed70a832a738d9b1dbcac76f05472144f1e1f3
Willen Garcia <willen.garcia05@gmail.com>
2024-09-20 22:17:35 -0300

    corrigindo o input:name=image

--- Trello/frontend/src/Componentes/adicionarImagens.js
+++ Trello/frontend/src/Componentes/adicionarImagens.js
@@ -86,6 +86,7 @@ function AdicionarImagens() {
                 <form onSubmit={insertImagens}>
                     <input
                         type='file'
+                        name='image'
                         onChange={(e) => setImagens(e.target.files[0])}
                     />
                     <button type='submit'>Upload</button>
