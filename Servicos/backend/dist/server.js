4bbd1a49f9463746e113261bd062ddb5ae7967e7
Willen Garcia <89990936+willengarcia@users.noreply.github.com>
2024-09-27 18:02:49 -0300

    fazendo build

--- Servicos/backend/dist/server.js
+++ Servicos/backend/dist/server.js
@@ -7,25 +7,16 @@ const express_1 = __importDefault(require("express"));
 require("express-async-errors");
 const cors_1 = __importDefault(require("cors"));
 const routes_1 = require("./routes");
+require("dotenv/config");
 const app = (0, express_1.default)();
-app.use((0, cors_1.default)({
-    origin: 'http://localhost:3000', // Remova a barra final
+const porta = process.env.PORT || 4000;
+const corsOptions = {
+    origin: 'https://servicos-bice.vercel.app', // Remova a barra final
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
-}));
+};
+app.use((0, cors_1.default)(corsOptions));
 app.use(express_1.default.json());
 app.use(routes_1.rotas);
-app.use((err, req, res, next) => {
-    if (err instanceof Error) {
-        // se for uma instancia do tipo erro
-        return res.status(400).json({
-            error: err.message
-        });
-    }
-    return res.status(500).json({
-        status: 'error',
-        message: 'Erro Interno'
-    });
-});
-app.listen(3333, () => { console.log('Servidor Online'); });
+app.listen(porta, () => { console.log('Servidor Online'); });
 //# sourceMappingURL=server.js.map
