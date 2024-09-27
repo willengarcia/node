0253161d4b16e3dbf20bfcdfe5018dbd4b2a34ca
Willen Garcia <89990936+willengarcia@users.noreply.github.com>
2024-09-27 18:00:28 -0300

    mudando algumas config do server

--- Servicos/backend/src/server.ts
+++ Servicos/backend/src/server.ts
@@ -2,28 +2,17 @@ import express, {Request, Response, NextFunction} from 'express'
 import 'express-async-errors'
 import cors from 'cors';
 import {rotas} from './routes'
+import 'dotenv/config';
 
 const app = express()
-
-app.use(cors({
+const porta = process.env.PORT || 4000
+const corsOptions = {
     origin: 'https://servicos-bice.vercel.app', // Remova a barra final
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials:true
-}))
+};
+app.use(cors(corsOptions))
 app.use(express.json())
 app.use(rotas)
 
-app.use((err: Error, req: Request, res: Response, next: NextFunction)=>{
-    if(err instanceof Error){
-        // se for uma instancia do tipo erro
-        return res.status(400).json({
-            error: err.message
-        })
-    }
-    return res.status(500).json({
-        status: 'error',
-        message: 'Erro Interno'
-    })
-})
-
-app.listen(process.env.PORT, ()=>{console.log('Servidor Online')})
+app.listen(porta, ()=>{console.log('Servidor Online')})
