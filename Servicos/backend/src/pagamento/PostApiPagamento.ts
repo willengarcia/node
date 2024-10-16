176d6da868eaab9375d9d38435e3d241ee07a2ce
Willen Garcia <89990936+willengarcia@users.noreply.github.com>
2024-10-16 07:42:00 -0300

    testando rota post

--- Servicos/backend/src/pagamento/PostApiPagamento.ts
+++ Servicos/backend/src/pagamento/PostApiPagamento.ts
@@ -4,21 +4,31 @@ import axios from 'axios'
 class PostApiPagamento{
     async handle(req:Request, res:Response){
         try{
-            if(req.body.action === 'payment.updated'){
+            if(req.body.action === 'payment.updated') {
                 await axios({
-                    url: 'https://services-zeta-gold.vercel.app/returnPagamento',
-                    headers:{
-                        paymentid:req.body.data.id
-                    }
+                  url: '/returnPagamento',
+                  headers: {
+                    paymentid: req.body.data.id
+                  }
                 })
-                .then(x=>x.data)
-                .then(async (r)=>{
-                    if(r.response.status==='approved'){
-                        console.log(r)
-                        return r
-                    }
+                .then(x => x.data)
+                .then(async (r) => {
+                  // Verifica se r e r.response existem antes de acessar r.response.status
+                  if (r && r.response && r.response.status === 'approved') {
+                    console.log(r);
+                    return r;
+                  } else {
+                    console.error('Pagamento não aprovado ou resposta inválida');
+                    return { error: 'Pagamento não aprovado ou resposta inválida' };
+                  }
                 })
-                .catch(console.log)
+                .catch((err) => {
+                  console.error('Erro ao acessar o pagamento:', err);
+                  return { error: 'Erro ao acessar o pagamento' };
+                });
+            } else {
+                console.error('Ação não é "payment.updated"');
+                return { error: 'Ação inválida' };
             }
         }catch(err){
             throw new Error('Erro: '+err)
