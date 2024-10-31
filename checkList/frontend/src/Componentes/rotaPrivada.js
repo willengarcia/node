a84a22f8b4a475dffe6d4138ca3a88088746763d
Willen Garcia <89990936+willengarcia@users.noreply.github.com>
2024-10-30 22:26:46 -0300

    colocando a api no env

--- checkList/frontend/src/Componentes/rotaPrivada.js
+++ checkList/frontend/src/Componentes/rotaPrivada.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
-
 const PrivateRoute = ({ children }) => {
   const token = localStorage.getItem('authToken'); // Verifica se o token está armazenado
 
